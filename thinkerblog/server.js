cdd544876c8812eef5972905280a59d417dba93c
Thinkeraty <contact.thinkeraty@gmail.com>
2020-03-24 20:33:19 +0530

    nothing

--- thinkerblog/server.js
+++ thinkerblog/server.js
@@ -20,4 +20,6 @@ app.get('/', async (req, res) => {
 
 app.use('/articles', articleRouter)
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,  () => console.log(`Server running on port ${PORT}`));
